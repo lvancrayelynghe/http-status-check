656bbeec2c0829421e56678230cdc4be424db094
Benoth <benoth83@gmail.com>
2016-11-17 18:55:21 +0100

    First usable prototype

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # SEO redirects checker
 
-Exemple Go project
+## Installation
+
+### Go
+
+```
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source ~/.gvm/scripts/gvm
+sudo apt-get install bison
+gvm install go1.4 -B
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.7 --prefer-binary
+gvm use go1.7 --default
+```
+
+### Packages
+
+```
+go get github.com/parnurzeal/gorequest
+go get gopkg.in/go-playground/pool.v3
+```
--- main.go
+++ main.go
@@ -2,61 +2,52 @@
 // https://www.golang-book.com/books/intro
 
 // go get github.com/parnurzeal/gorequest
-// go get github.com/PuerkitoBio/goquery
 // go get gopkg.in/go-playground/pool.v3
 
 package main
 
 import (
     "log"
-    // "fmt"
+    "io"
+    "os"
+    "encoding/csv"
+    "strconv"
     "time"
     "net/http"
     "strings"
-    // "sync"
     "github.com/parnurzeal/gorequest"
-    "github.com/PuerkitoBio/goquery"
     "gopkg.in/go-playground/pool.v3"
 )
 
 type Url struct {
     uri string
-    visited bool
     response gorequest.Response
-    body string
     duration time.Duration
 }
 
 var visitedURL map[string]Url = make(map[string]Url)
-var request = gorequest.New().Timeout(1000 * time.Millisecond)
+var request = gorequest.New().Timeout(10000 * time.Millisecond)
 
 var baseURL string = "http://www.exemple.com/"
+var replaceURL string = "https://recette.exemple.com/"
 
+var urls = make([]string, 0)
 
 func main() {
     log.Println("Starting...")
 
+    err := readCSV("input.csv")
+    if err != nil {
+        log.Fatalln(err)
+    }
 
     p := pool.NewLimited(10)
     batch := p.Batch()
     defer p.Close()
 
-
-    url := Url{uri: baseURL}
-    log.Println("Parsing " + url.uri)
-    url.parseUrl()
-    visitedURL[baseURL] = url
-
     go func() {
-        // Copy the map to avoid overwriting
-        work := make(map[string]Url)
-        for k,v := range visitedURL {
-            work[k] = v
-        }
-        for _, newUrl := range work {
-            if newUrl.visited == false {
-                batch.Queue(handleUrl(newUrl))
-            }
+        for _, url := range urls {
+            batch.Queue(handleUrl(url))
         }
 
         // DO NOT FORGET THIS OR GOROUTINES WILL DEADLOCK
@@ -64,100 +55,136 @@ func main() {
         batch.QueueComplete()
     }()
 
-
     for crawl := range batch.Results() {
         if err := crawl.Error(); err != nil {
             // handle error
+            log.Println("Error: ", err)
         }
 
-        // use return value (url object)
-        // log.Println(crawl.Value())
+        url := crawl.Value().(Url)
+
+        visitedURL[url.uri] = url
     }
 
-    nop := 0
-    yep := 0
-    for _, newUrl := range visitedURL {
-        if newUrl.visited == false {
-            nop += 1
-        } else {
-            yep += 1
-        }
-    }
+    writeCSV("output.csv", visitedURL)
 
-    // log.Println(visitedURL[baseURL])
-    log.Println(nop)
-    log.Println(yep)
-    log.Println(len(visitedURL))
+    log.Println("Done !")
 }
 
-func handleUrl(url Url) pool.WorkFunc  {
+func readCSV(filepath string) error {
+    file, err := os.Open(filepath)
+    if err != nil {
+        return err
+    }
+
+    // automatically call Close() at the end of current method
+    defer file.Close()
+
+    // options are available at: http://golang.org/src/pkg/encoding/csv/reader.go?s=3213:3671#L94
+    reader := csv.NewReader(file)
+    reader.Comma = ';'
+
+    for {
+        // read just one record, but we could ReadAll() as well
+        record, err := reader.Read()
+
+        // end-of-file is fitted into err
+        if err == io.EOF {
+            break
+        } else if err != nil {
+            return err
+        }
+
+        urls = append(urls, record[0])
+    }
+
+    return nil
+}
+
+func writeCSV(filepath string, datas map[string]Url) error {
+    file, err := os.Create(filepath)
+    if err != nil {
+        log.Fatal("Cannot create file", err)
+    }
+
+    defer file.Close()
+
+    writer := csv.NewWriter(file)
+    writer.Comma = ';'
+
+    line := []string{"Url", "Response Code", "Duration", "Redirect"}
+    errWrite := writer.Write(line)
+    if errWrite != nil {
+        log.Fatal("Cannot create file", errWrite)
+    }
+
+    for _, url := range datas {
+        responseCode := url.response.StatusCode
+        duration := url.duration.String()
+
+        locationValue, locationExist := url.response.Header["Location"]
+        location := ""
+        if locationExist {
+            location = locationValue[0]
+        }
+
+        line := []string{url.uri, strconv.Itoa(responseCode), duration, location}
+
+        errWrite := writer.Write(line)
+        if errWrite != nil {
+            log.Fatal("Cannot create file", errWrite)
+        }
+    }
+
+    defer writer.Flush()
+
+    return nil
+}
+
+func handleUrl(url string) pool.WorkFunc  {
     return func(wu pool.WorkUnit) (interface{}, error) {
         if wu.IsCancelled() {
             // return values not used
             return nil, nil
         }
 
-        log.Println("Parsing " + url.uri)
+        // Change destination URL
+        url = strings.Replace(url, baseURL, replaceURL, -1)
 
-        url.parseUrl()
+        log.Println("Parsing " + url)
 
-        visitedURL[url.uri] = url
+        newUrl := Url{uri: url}
+        errs := newUrl.parseUrl()
 
-        return url, nil // everything ok, send nil as 2nd parameter if no error
+        if errs != nil {
+            return nil, errs[0]
+        }
+
+        return newUrl, nil // everything ok, send nil as 2nd parameter if no error
     }
 }
 
-func getUrl(url string) (gorequest.Response, string, time.Duration, []error) {
+func getUrl(url string) (gorequest.Response, time.Duration, []error) {
     timeStart := time.Now()
 
-    response, body, err := request.
+    response, _, err := request.
         Get(url).
         RedirectPolicy(func(req gorequest.Request, via []gorequest.Request) error {
             return http.ErrUseLastResponse
         }).End()
 
-    return response, body, time.Since(timeStart), err
+    return response, time.Since(timeStart), err
 }
 
 func (url *Url) parseUrl() ([]error) {
-    response, body, duration, requestError := getUrl(url.uri)
+    response, duration, requestError := getUrl(url.uri)
 
     url.response = response
-    url.body = body
     url.duration = duration
-    url.visited = true
 
     if len(requestError) > 0 {
         return requestError
     }
-
-    responseCode := response.StatusCode
-    location, locationExist := response.Header["Location"]
-
-    if responseCode == 301 && locationExist {
-        location := location[0]
-
-        visitedURL[location] = Url{uri: location}
-
-        return nil
-    }
-
-    // Parse
-    doc, parserErr := goquery.NewDocumentFromResponse(response)
-    if parserErr != nil {
-        errors := []error{parserErr}
-        return errors
-    }
-
-    // Find the review items
-    // log.Println(doc.Find("title").Text())
-    doc.Find("a").Each(func(i int, element *goquery.Selection) {
-        link, linkExist := element.Attr("href")
-        _, exists := visitedURL[link]
-        if linkExist && link != "#" && strings.HasPrefix(link, baseURL) && ! exists {
-            visitedURL[link] = Url{uri: link}
-        }
-    })
 
     return nil
 }
