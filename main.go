afec077f350402c42ca88ef1d0fc0ea15220b82c
Benoth <benoth83@gmail.com>
2016-11-18 00:38:25 +0100

    Split code for readability

--- main.go
+++ main.go
@@ -2,23 +2,13 @@ package main
 
 import (
     "log"
-    "io"
     "os"
-    "strconv"
     "time"
     "net/url"
-    "net/http"
-    "encoding/csv"
     "github.com/jawher/mow.cli"
     "github.com/parnurzeal/gorequest"
     "gopkg.in/go-playground/pool.v3"
 )
-
-type Url struct {
-    uri string
-    response gorequest.Response
-    duration time.Duration
-}
 
 var urls = make([]string, 0)
 var visitedURL map[string]Url = make(map[string]Url)
@@ -105,76 +95,6 @@ func process(inputPath string, outputPath string, concurrency uint, newuri strin
     return nil
 }
 
-func readCSV(filepath string) error {
-    file, err := os.Open(filepath)
-    if err != nil {
-        return err
-    }
-
-    // automatically call Close() at the end of current method
-    defer file.Close()
-
-    // options are available at: http://golang.org/src/pkg/encoding/csv/reader.go?s=3213:3671#L94
-    reader := csv.NewReader(file)
-    reader.Comma = ';'
-
-    for {
-        // read just one record, but we could ReadAll() as well
-        record, err := reader.Read()
-
-        // end-of-file is fitted into err
-        if err == io.EOF {
-            break
-        } else if err != nil {
-            return err
-        }
-
-        urls = append(urls, record[0])
-    }
-
-    return nil
-}
-
-func writeCSV(filepath string, datas map[string]Url) error {
-    file, err := os.Create(filepath)
-    if err != nil {
-        log.Fatal("Cannot create file", err)
-    }
-
-    defer file.Close()
-
-    writer := csv.NewWriter(file)
-    writer.Comma = ';'
-
-    line := []string{"Url", "Response Code", "Duration", "Redirect"}
-    errWrite := writer.Write(line)
-    if errWrite != nil {
-        log.Fatal("Cannot create file", errWrite)
-    }
-
-    for _, currentUrl := range datas {
-        responseCode := currentUrl.response.StatusCode
-        duration := currentUrl.duration.String()
-
-        locationValue, locationExist := currentUrl.response.Header["Location"]
-        location := ""
-        if locationExist {
-            location = locationValue[0]
-        }
-
-        line := []string{currentUrl.uri, strconv.Itoa(responseCode), duration, location}
-
-        errWrite := writer.Write(line)
-        if errWrite != nil {
-            log.Fatal("Cannot create file", errWrite)
-        }
-    }
-
-    defer writer.Flush()
-
-    return nil
-}
-
 func handleUrl(currentUrl string, newuri string) pool.WorkFunc  {
     return func(wu pool.WorkUnit) (interface{}, error) {
         if wu.IsCancelled() {
@@ -209,29 +129,4 @@ func handleUrl(currentUrl string, newuri string) pool.WorkFunc  {
 
         return newUrl, nil // everything ok, send nil as 2nd parameter if no error
     }
-}
-
-func getUrl(currentUrl string) (gorequest.Response, time.Duration, []error) {
-    timeStart := time.Now()
-
-    response, _, err := request.
-        Get(currentUrl).
-        RedirectPolicy(func(req gorequest.Request, via []gorequest.Request) error {
-            return http.ErrUseLastResponse
-        }).End()
-
-    return response, time.Since(timeStart), err
-}
-
-func (currentUrl *Url) parseUrl() ([]error) {
-    response, duration, requestError := getUrl(currentUrl.uri)
-
-    currentUrl.response = response
-    currentUrl.duration = duration
-
-    if len(requestError) > 0 {
-        return requestError
-    }
-
-    return nil
 }
